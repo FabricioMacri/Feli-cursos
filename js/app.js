4780b7b3600ea6c4f30366b63589748ca52816cb
Fabricio Macri <fabriciomacri@hotmail.com>
2024-02-11 17:11:31 -0300

    preview terminada

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(async () => {
+    const { value: email } = await Swal.fire({
+      title: "Ingrese su email para obtener beneficios",
+      input: "email",
+      inputPlaceholder: "Ingrese su dirección de correo electrónico.",
+      showCancelButton: true
+    });
+    if (email) {
+      Swal.fire(`Entered email: ${email}`);
+    }
+  })()
